dcfa07808ca2039aaadd74b71f38a047ffa52c11
joshktatum <joshktatum@gmail.com>
2018-11-13 10:55:42 -0500

    added led code

---
+++ fillingStation/sketch_nov13a/sketch_nov13a.ino
@@ -0,0 +1,29 @@
+/* 
+  IR Breakbeam sensor demo!
+*/
+ 
+ 
+#define SENSORPIN 4
+ 
+// variables will change:
+int sensorState = 0, lastState=0;         // variable for reading the pushbutton status
+ 
+void setup() {
+  Serial.begin(9600);
+  pinMode(SENSORPIN, INPUT);     
+  digitalWrite(SENSORPIN, HIGH); // turn on the pullup
+}
+ 
+void loop(){
+  // read the state of the pushbutton value:
+  sensorState = digitalRead(SENSORPIN);
+ 
+  // check if the sensor beam is broken
+  // if it is, the sensorState is LOW:
+  if (sensorState == LOW) {     
+    Serial.println("Broken");
+  } 
+  else {
+    Serial.println("Unbroken");
+  }
+}
